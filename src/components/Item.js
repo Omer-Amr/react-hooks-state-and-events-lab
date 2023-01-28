e71c6ab5565af8f5277823da845daa5d8317453b
omer <you@example.com>
2023-01-28 14:57:40 +0300

    completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [added, itemIsAdded] = useState(true)
+  function handleClick(){
+    itemIsAdded((added)=>!added)
+  }
+  const addItem = added?'':'in-cart'
+ 
   return (
-    <li className="">
+    <li className={addItem}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}> {added?'Add to Cart': "Remove From Cart"}</button>
     </li>
   );
 }
